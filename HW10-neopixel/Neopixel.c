bd94310093a303c93a04e7a2904b9f28437525af
Julian Delgado <126289439+jugadel1@users.noreply.github.com>
2023-05-16 20:05:05 -0500

    started neopixel

---
+++ HW10-neopixel/Neopixel.c
@@ -0,0 +1,36 @@
+#include "nu32dip.h" // constants, functions for startup and UART
+#include "ws2812b.h"
+
+// Constants for Neopixel Strip
+#define LEDS 8 // 8 per strip
+#define Sat 1 // max saturation
+#define Bri 1 // masx brightness
+
+// Function Prototypes
+void delay(int t);
+
+int main(void) {
+	NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
+	ws2812b_setup(); // Setup TMR2 for Neopixel Control
+	
+	float hue_iter = 0;
+	float saturation = Sat;
+	float brightness = Bri;
+
+	while(1){
+		wsColor color;
+		color = HSBtoRGB(hue_iter, saturation, brightness);
+		ws2812b_setColor(color, LEDS);
+		delay(100);
+
+		hue_iter++;
+		if (hue_iter==360) hue_iter = 0;
+	}
+	
+}
+
+void delay(int t) {
+  for (int i = 0; i < 12000*t; i++) {  // delays for time t in ms
+      _nop(); //do nothing
+  }
+}
--- HW10-neopixel/ws2812b.c
+++ HW10-neopixel/ws2812b.c
@@ -9,15 +9,15 @@
 
 // setup Timer2 for 48MHz, and setup the output pin
 void ws2812b_setup() {
+    __builtin_disable_interrupts();
     T2CONbits.TCKPS = 0; // Timer2 prescaler N=1 (1:1)
     PR2 = 65535; // maximum period, largest value that can go into delay_times
     TMR2 = 0; // initialize Timer2 to 0
-    T2CONbits.ON = 1; // turn on Timer2
-
+    
     // initialize output pin as off
-    // TRIS...
-    // LAT...
-    LATBbits.LATB6 = 0;
+    TRISBbits.TRISB6 = 0;  // TRIS...
+    LATBbits.LATB6 = 0;    // LAT...
+    __builtin_enable_interrupts();
 }
 
 // build an array of high/low times from the color input array, then output the high/low bits
